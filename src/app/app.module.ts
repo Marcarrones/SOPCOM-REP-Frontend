934255dd631fd7fdaa4ed58f708574d0738090a6
Carlos-PG <carlos.plana.garcia@estudiantat.upc.edu>
2022-05-17 09:56:42 +0200

    Project created and added angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CriterionComponent } from './criterion/criterion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CriterionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
