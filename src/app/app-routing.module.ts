e9f8e63ee1a13ce9d0abb5b7f163d13c6c098b51
Marc Duch Buechler <marcduchb20@gmail.com>
2024-10-12 17:06:22 +0200

    deleted unused routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -60,30 +60,19 @@ export class ConfirmDeactivateGuardM implements CanDeactivate<MapComponent> {
 const routes: Routes = [
   {path: 'method-chunk/:id', component: MethodChunkComponent, canDeactivate:[ConfirmDeactivateGuardMC]},
   {path: 'method-chunk', component: MethodChunkComponent, canDeactivate:[ConfirmDeactivateGuardMC]},
-  //{path: 'method-chunks', component: MethodChunkListComponent},
   {path: 'tool/:id', component: MethodElementDetailComponent, data: {type: 1, typeStr: "tool"}, canDeactivate:[ConfirmDeactivateGuardME]},
   {path: 'tool', component: MethodElementDetailComponent, data: {type: 1, typeStr: "tool"}, canDeactivate:[ConfirmDeactivateGuardME]},
-  //{path: 'tools', component: MethodElementListComponent, data: {type: 1, typeStr: "tool"}},
   {path: 'artefact/:id', component: MethodElementDetailComponent, data: {type: 2, typeStr: "artefact"}, canDeactivate:[ConfirmDeactivateGuardME]},
   {path: 'artefact', component: MethodElementDetailComponent, data: {type: 2, typeStr: "artefact"}, canDeactivate:[ConfirmDeactivateGuardME]},
-  //{path: 'artefacts', component: MethodElementListComponent, data: {type: 2, typeStr: "artefact"}},
   {path: 'activity/:id', component: MethodElementDetailComponent, data: {type: 3, typeStr: "activity"}, canDeactivate:[ConfirmDeactivateGuardME]},
   {path: 'activity', component: MethodElementDetailComponent, data: {type: 3, typeStr: "activity"}, canDeactivate:[ConfirmDeactivateGuardME]},
-  //{path: 'activities', component: MethodElementListComponent, data: {type: 3, typeStr: "activity"}},
   {path: 'role/:id', component: MethodElementDetailComponent, data: {type: 4, typeStr: "role"}, canDeactivate:[ConfirmDeactivateGuardME]},
   {path: 'role', component: MethodElementDetailComponent, data: {type: 4, typeStr: "role"}, canDeactivate:[ConfirmDeactivateGuardME]},
-  //{path: 'roles', component: MethodElementListComponent, data: {type: 4, typeStr: "role"}},
   {path: 'criterion/:id', component: CriterionDetailComponent, canDeactivate:[ConfirmDeactivateGuardC]},
   {path: 'criterion', component: CriterionDetailComponent, canDeactivate:[ConfirmDeactivateGuardC]},
-  //{path: 'criterions', component: CriterionListComponent},
-  //{path: 'map/:id', component: MapComponent, canDeactivate:[ConfirmDeactivateGuardM]},
-  //{path: 'map', component: MapComponent, canDeactivate:[ConfirmDeactivateGuardM]},
   {path: 'map/:id', component: MapComponent},
   {path: 'map', component: MapComponent},
-  //maps
-  {path: '**', redirectTo: 'method-chunk', pathMatch: 'full'},
-  //??
-  
+  {path: '**', redirectTo: 'method-chunk', pathMatch: 'full'},  
 ];
 
 @NgModule({
